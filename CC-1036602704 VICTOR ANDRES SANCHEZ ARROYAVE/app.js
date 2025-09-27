cd9fba17e581393cbb43448f8de00537d338ebcf
andrescesde1986 <vasancheza86@gmail.com>
2025-09-27 14:59:53 -0500

    Fase 2: Implementadas funciones para alimentar y jugar con la mascota

--- CC-1036602704 VICTOR ANDRES SANCHEZ ARROYAVE/app.js
+++ CC-1036602704 VICTOR ANDRES SANCHEZ ARROYAVE/app.js
@@ -5,4 +5,28 @@ let nivelFelicidad = 50;
 function mostrarEstado() {
   console.log(`Mascota: ${nombreMascota}, Hambre: ${nivelHambre}/100, Felicidad: ${nivelFelicidad}/100.`);
 }
- mostrarEstado();
+
+
+const alimentar = function(puntosComida) {
+  nivelHambre -= puntosComida;  
+  if (nivelHambre < 0) {
+    nivelHambre = 0;  
+  }
+  console.log(`¡Ñam! El hambre de ${nombreMascota} ha disminuido. Hambre: ${nivelHambre}/100.`);
+};
+
+
+const jugar = (puntosDiversion) => {
+  nivelFelicidad += puntosDiversion;  
+  if (nivelFelicidad > 100) {
+    nivelFelicidad = 100; 
+  }
+  console.log(`¡Qué divertido! La felicidad de ${nombreMascota} ha aumentado. Felicidad: ${nivelFelicidad}/100.`);
+};
+
+
+mostrarEstado();   
+alimentar(30);     
+mostrarEstado();  
+jugar(20);         
+mostrarEstado();   
