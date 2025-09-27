b0d4d6b54e25a51dc5df5955cc46d15ae5e0497e
andrescesde1986 <vasancheza86@gmail.com>
2025-09-27 15:01:42 -0500

    Fase 3: Añadida simulación de tiempo y diagnóstico de la mascota

--- CC-1036602704 VICTOR ANDRES SANCHEZ ARROYAVE/app.js
+++ CC-1036602704 VICTOR ANDRES SANCHEZ ARROYAVE/app.js
@@ -19,14 +19,55 @@ const alimentar = function(puntosComida) {
 const jugar = (puntosDiversion) => {
   nivelFelicidad += puntosDiversion;  
   if (nivelFelicidad > 100) {
-    nivelFelicidad = 100; 
+    nivelFelicidad = 100;  
   }
   console.log(`¡Qué divertido! La felicidad de ${nombreMascota} ha aumentado. Felicidad: ${nivelFelicidad}/100.`);
 };
 
 
-mostrarEstado();   
-alimentar(30);     
+const simularPasoDelTiempo = () => {
+  for (let i = 0; i < 4; i++) {
+    nivelHambre += 10; 
+    nivelFelicidad -= 5;  
+
+   
+    if (nivelHambre > 100) nivelHambre = 100;
+    if (nivelFelicidad < 0) nivelFelicidad = 0;
+  }
+
+  console.log("Han pasado algunas horas...");
+  mostrarEstado();  
+};
+
+
+function diagnosticoMascota() {
+ 
+  if (nivelHambre < 30) {
+    console.log("Tu mascota está satisfecha.");
+  } else if (nivelHambre < 60) {
+    console.log("Tu mascota tiene algo de hambre.");
+  } else {
+    console.log("¡Alerta! Tu mascota está muy hambrienta.");
+  }
+
+ 
+  switch (true) {
+    case nivelFelicidad >= 70:
+      console.log("Tu mascota está muy feliz y juguetona.");
+      break;
+    case nivelFelicidad >= 40:
+      console.log("Tu mascota está contenta.");
+      break;
+    default:
+      console.log("Tu mascota se siente un poco triste.");
+      break;
+  }
+}
+
+
 mostrarEstado();  
+alimentar(30);     
 jugar(20);         
-mostrarEstado();   
+simularPasoDelTiempo();  
+diagnosticoMascota();    
+  
